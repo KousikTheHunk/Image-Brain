01db6e7773aa2f8e2d860db67037985811cc3937
kousik1097 <kousik.chandra@yahoo.com>
2019-10-25 12:36:28 +0530

    first commit in Github

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = ({onRouteChange}) => {
+	return (
+		<div>
+			<footer>
+			<p className='pa3'> All Rights Reserved &copy; 2019 <a className='black' href='https://www.linkedin.com/in/kousik-chandra-1a5509122'>Kousik Chandra.</a> </p>
+			</footer>
+		</div>
+	);
+}
+
+
+export default Footer;
